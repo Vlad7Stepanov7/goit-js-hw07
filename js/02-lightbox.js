35a5f837c665f12604cc6a0b72254832dbbb383f
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-08-28 20:19:24 +0300

    убрал лишний код

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,10 +3,12 @@ import { galleryItems } from './gallery-items.js';
 
 const murkupImage = galleryItems => {
     const { preview, original, description } = galleryItems;
-    return `
+  return `
+    <li>
   <a class="gallery__link" rel="rel2" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
   </a>
+  </li>
     `;
 };
 // добавил разметку галереи
@@ -19,13 +21,4 @@ gallery.insertAdjacentHTML(`afterbegin`, galleryMurkup);
 // инициализация библиотеки
 const lightbox = new SimpleLightbox(`.gallery__link`, { captionsData: `title`, captionPosition: `bottom`, captionDelay: 250, });
 
-// открытие модалки по клику
-gallery.addEventListener(`click`, (e) => {
-  e.preventDefault();
-  
-    if (e.target.nodeName !== `IMG`) {
-        return
-    }
 
-    lightbox.open(`.gallery__link`);
-});
